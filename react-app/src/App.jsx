ceaeaf2f3dcfd5387a96f5590c1a702ba1ecee04
shanmugapriya222 <shanmugapriyab211@gmail.com>
2025-02-18 12:33:36 +0530

    commited

---
+++ react-app/src/App.jsx
@@ -0,0 +1,17 @@
+// client/src/App.jsx
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="hero">
+        <h1>Terrible LifeHacks</h1>
+        <p>Questionable Tips.Odd Solutions.Awful Tricks</p>
+        <button>Explore Gallery</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
